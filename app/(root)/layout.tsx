dadcb157b47066552f621c27e1afbbc593a90297
OtaraAlex <mbechealex61@gmail.com>
2024-05-08 11:48:16 +0300

    feat: Implement the home page UI and add new pages for MyBanks, PaymentTransfer, and TransactionHistory.
    These pages are necessary for the application's navigation and functionality.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,27 @@
+import Image from "next/image";
+
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "Alex", lastName: "EG" };
   return (
-    <main>
-      SIDEBAR
-      {children}
+    <main className="flex w-full h-screen font-inter">
+      <Sidebar user={loggedIn} />
+
+      <div className="flex flex-col size-full">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" alt="logo" width={30} height={30} />
+          <div className="">
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
