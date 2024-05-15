2760f63142e86349d732a3e4465e7e02b92cee44
OtaraAlex <mbechealex61@gmail.com>
2024-05-15 21:00:59 +0300

    Implement My Banks page

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -1,16 +1,19 @@
 import { formatAmount } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
+import React from "react";
+import Copy from "./Copy";
 
-const BankCard = ({ account, userName, showBalance }: CreditCardProps) => {
+const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) => {
   return (
     <div className="flex flex-col">
-      <Link href="/" className="bank-card">
+      <Link
+        href={`/transaction-history/?id=${account.appwriteItemId}`}
+        className="bank-card"
+      >
         <div className="bank-card_content">
           <div>
-            <h1 className="text-16 font-semibold text-white">
-              {userName}
-            </h1>
+            <h1 className="text-16 font-semibold text-white">{account.name}</h1>
             <p className="font-ibm-plex-serif font-black text-white">
               {formatAmount(account.currentBalance)}
             </p>
@@ -22,7 +25,7 @@ const BankCard = ({ account, userName, showBalance }: CreditCardProps) => {
               <h2 className="text-12 font-semibold text-white">●● / ●●</h2>
             </div>
             <p className="text-14 font-semibold tracking-[1.1px] text-white">
-              ●●●● ●●●● ●●●● <span className="text-16">1234</span>
+              ●●●● ●●●● ●●●● <span className="text-16">{account?.mask}</span>
             </p>
           </article>
         </div>
@@ -47,7 +50,7 @@ const BankCard = ({ account, userName, showBalance }: CreditCardProps) => {
         />
       </Link>
 
-      {/* COPY */}
+      {showBalance && <Copy title={account?.sharableId} />}
     </div>
   );
 };
