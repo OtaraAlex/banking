d1b82fbdfda33b85d59b0b7cda4985ea39390049
OtaraAlex <mbechealex61@gmail.com>
2024-05-16 08:39:22 +0300

    Implement Transaction History Page

--- components/RecentTransactions.tsx
+++ components/RecentTransactions.tsx
@@ -16,7 +16,7 @@ const RecentTransactions = ({
       <header className="flex items-center justify-between">
         <h2 className="recent-transactions-label">Recent Transactions</h2>
         <Link
-          href={`/transactions-history/?id=${appwriteItemId}`}
+          href={`/transaction-history/?id=${appwriteItemId}`}
           className="view-all-btn"
         >
           View all
